790bf5ceda5c195b40296f546984286399d7fec8
Gregor Gatej <gregor.gatej@gmail.com>
2026-02-07 09:33:08 +0100

    feat: add test for #valid_move?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,24 +1,29 @@
 require_relative "../lib/connect_four"
 
 describe ConnectFour::Board do
+  subject(:board)  { described_class.new }
   describe "#initialize" do
     it "creates a 6x7 board" do
-      board = ConnectFour::Board.new
       expect(board.state.length).to eq(6)
       expect(board.state[0].length).to eq(7)
     end
 
     it "fills the board with empty spaces" do
-      board = ConnectFour::Board.new
       expect(board.state.flatten.all? { |cell| cell == " " }).to be true
     end
   end
 
   describe "#to_s" do
     it "returns a string representation of the board" do
-      board = ConnectFour::Board.new
       expect(board.to_s).to include("|")
       expect(board.to_s).to include("+")
+    end
+  end
+
+  describe "#valid_move?" do
+    it "returns true for empty columns in range" do
+      expect(board.valid_move?(0)).to be true
+      expect(board.valid_move?(6)).to be true
     end
   end
 end
