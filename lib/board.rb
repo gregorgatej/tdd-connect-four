465a1e162156a46d8047811af0b89d5007abeb9b
Gregor Gatej <gregor.gatej@gmail.com>
2026-02-07 07:39:50 +0100

    feat: implement #initialize to pass first Board test

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,12 @@
 module ConnectFour
   class Board
-    
+    ROWS = 6
+    COLS = 7
+
+    attr_accessor :state
+
+    def initialize
+      @state = Array.new(ROWS) { Array.new(COLS, " ") }
+    end
   end
 end
