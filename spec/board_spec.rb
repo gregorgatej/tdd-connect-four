43531fe39f783c4933e6a1901c8062e383d3c54a
Gregor Gatej <gregor.gatej@gmail.com>
2026-02-07 09:10:36 +0100

    feat: add second #initialize test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,5 +7,10 @@ describe ConnectFour::Board do
       expect(board.state.length).to eq(6)
       expect(board.state[0].length).to eq(7)
     end
+
+    it "fills the board with empty spaces" do
+      board = ConnectFour::Board.new
+      expect(board.state.flatten.all? { |cell| cell == " " }).to be true
+    end
   end
 end
