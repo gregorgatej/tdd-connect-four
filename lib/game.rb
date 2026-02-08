b7aece61da9b7193903e9b10da5787d4c4c70ff1
Gregor Gatej <gregor.gatej@gmail.com>
2026-02-08 11:39:21 +0100

    feat: implement #initialize for Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,11 @@
 module ConnectFour
   class Game
-    
+    attr_reader :player_red, :player_yellow, :board
+
+    def initialize
+      @player_red = Player.new("🔴")
+      @player_yellow = Player.new("🟡")
+      @board = Board.new
+    end
   end
 end
