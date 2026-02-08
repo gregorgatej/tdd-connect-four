8b4203769cb2405a1fac93c81b168962a238aa96
Gregor Gatej <gregor.gatej@gmail.com>
2026-02-08 11:24:16 +0100

    feat: implement Player class

---
+++ lib/player.rb
@@ -0,0 +1,13 @@
+module ConnectFour
+  class Player
+    attr_accessor :mark
+
+    def initialize(mark)
+      @mark = mark
+    end
+
+    def to_s
+      "Player with mark \"#{mark}\""
+    end
+  end
+end
